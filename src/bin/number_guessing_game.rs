dc75127ce027c24fd2c519344050fd98f7e7c0eb
William Wang <williamw0825@gmail.com>
2023-12-10 17:14:45 +0800

    feat: refactor number_guessing_game

--- src/bin/number_guessing_game.rs
+++ src/bin/number_guessing_game.rs
@@ -1,6 +1,5 @@
 use std::cmp::Ordering;
-use std::io;
-use std::io::Write;
+use std::io::{self, Write};
 
 use rand::Rng;
 
@@ -8,6 +7,7 @@ use rand::Rng;
 mod tests {
     use crate::{do_guess, new_game};
 
+    // start of basic use cases
     #[test]
     fn new_game_ok() {
         let (min, max, secret_number) = new_game("1 100");
@@ -19,16 +19,18 @@ mod tests {
     #[test]
     fn do_guess_bingo() {
         let guess_result = do_guess(1, 100, 50, "50");
-        assert_eq!(guess_result, Ok((50, 50, 50)));
+        assert_eq!(guess_result, Ok((0, 50, 50)));
     }
+    // end of basic use cases
 
+    // start of exception use cases
     #[test]
     fn do_guess_wrong() {
         let guess_result = do_guess(1, 100, 37, "50");
-        assert_eq!(guess_result, Ok((50, 1, 49)));
+        assert_eq!(guess_result, Ok((1, 1, 49)));
 
         let guess_result = do_guess(1, 49, 37, "25");
-        assert_eq!(guess_result, Ok((25, 26, 49)));
+        assert_eq!(guess_result, Ok((-1, 26, 49)));
     }
 
     #[test]
@@ -39,6 +41,7 @@ mod tests {
         let guess_result = do_guess(25, 50, 37, "51");
         assert_eq!(guess_result.unwrap_err(), "Number should between 25 and 50");
     }
+    // end of exception use cases
 }
 
 fn main() -> io::Result<()> {
@@ -64,17 +67,17 @@ fn main() -> io::Result<()> {
 
         let guess_result = do_guess(min, max, secret_number, &input_str);
         match guess_result {
-            Ok((guess_num, new_min, new_max)) => {
-                print!("You guess {guess_num}, ");
+            Ok((result, new_min, new_max)) => {
+                print!("You guess {}, ", input_str.trim());
                 count += 1;
 
                 min = new_min;
                 max = new_max;
 
-                if new_min == new_max {
+                if result == 0 {
                     println!("You win with {count} guesses!");
                     break;
-                } else if guess_num < new_min {
+                } else if result == -1 {
                     println!("Too small!");
                 } else {
                     println!("Too big!");
@@ -89,6 +92,16 @@ fn main() -> io::Result<()> {
     Ok(())
 }
 
+/// 创建新的猜数字游戏，返回游戏范围以及范围内的随机数字。
+///
+/// # Arguments
+///
+/// * `game_str` - 游戏创建字符串，空格分割的数字起始和结束范围。
+///
+/// # Examples
+/// ```
+/// let (min, max, secret) = new_game("1 100")
+/// ```
 fn new_game(game_str: &str) -> (u32, u32, u32) {
     let range: Vec<u32> = game_str
         .split_whitespace()
@@ -102,7 +115,17 @@ fn new_game(game_str: &str) -> (u32, u32, u32) {
     (min, max, secret_number)
 }
 
-fn do_guess(min: u32, max: u32, secret_number: u32, guess_str: &str) -> Result<(u32, u32, u32), String> {
+/// 进行一轮猜数字游戏，返回猜测结果以及根据结果调整过后的数字范围。
+///
+/// 功能包含对输入进行校验。
+///
+/// # Arguments
+///
+/// * `min` - 数字范围：最小数字
+/// * `max` - 数字范围：最大数字
+/// * `secret_number` - 猜数字游戏答案
+/// * `guess_str` - 本轮游戏输入
+fn do_guess(min: u32, max: u32, secret_number: u32, guess_str: &str) -> Result<(i8, u32, u32), String> {
     match guess_str.trim().parse::<u32>() {
         Ok(_num) => {
             if _num < min || _num > max {
@@ -110,9 +133,9 @@ fn do_guess(min: u32, max: u32, secret_number: u32, guess_str: &str) -> Result<(
             }
             // 比对结果，调整数字范围
             match _num.cmp(&secret_number) {
-                Ordering::Less => Ok((_num, _num + 1, max)),
-                Ordering::Greater => Ok((_num, min, _num - 1)),
-                Ordering::Equal => Ok((_num, _num, _num)),
+                Ordering::Less => Ok((-1, _num + 1, max)),
+                Ordering::Greater => Ok((1, min, _num - 1)),
+                Ordering::Equal => Ok((0, _num, _num)),
             }
         }
         Err(_) => return Err(format!("Please input a integer number"))
